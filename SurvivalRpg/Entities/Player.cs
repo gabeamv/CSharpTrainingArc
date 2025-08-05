05c255e042bd187af8d7d68e34cd067b194d7e4d
gamvr <gabrielvillanueva11155@gmail.com>
2025-08-05 02:53:55 -0700

    implemented map which contains an enumeration of MapSymbols. char values of this enumeration will be used to launch certain events such as enemy encounters, item pick ups, etc.

--- SurvivalRpg/Entities/Player.cs
+++ SurvivalRpg/Entities/Player.cs
@@ -10,24 +10,18 @@ using System.Threading.Tasks;
 
 namespace SurvivalRpg.Entities
 {
-    public class Player : Entity
+    public abstract class Player : Entity
     {
+        public override int Health { get; set; } = 50;
+        public override int MaxHealth { get; set; } = 50;
         public MapService Map { get; private set; }
-        public Class PlayerClass { get; private set; }
         public MapService.Coord PlayerCoords { get; private set; }
+        
         public Player(MapService map, MapService.Coord playerCoords)
         {
             Map = map;
             PlayerCoords = playerCoords;
+        }
 
-        }
-        public override int Health 
-        { 
-            get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
-        }
-        public override int MaxHealth 
-        { 
-            get => throw new NotImplementedException(); set => throw new NotImplementedException(); 
-        }
     }
 }
