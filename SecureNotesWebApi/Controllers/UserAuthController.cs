525e7a77b5ff4fb6d20fd2be4c806243596734f0
gamvr <gabrielvillanueva11155@gmail.com>
2025-08-16 21:37:59 -0700

    began implementing back end with user registration and login

---
+++ SecureNotesWebApi/Controllers/UserAuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureNotesWebApi.Context;
+using SecureNotesWebApi.Models;
+namespace SecureNotesWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserAuthController : ControllerBase
+    {
+        private readonly SecureNotesContext _context;
+        public UserAuthController(SecureNotesContext context)
+        {
+            _context = context;
+        }
+
+        // Method to register a new user.
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserAuth userAuth)
+        {
+            var duplicateUser = await _context.UserAuths.FirstOrDefaultAsync(u => u.Username == userAuth.Username);
+            if (duplicateUser != null)
+            {
+                return Conflict($"There is already a user:\n{userAuth}");
+            }
+            await _context.UserAuths.AddAsync(userAuth);
+            await _context.SaveChangesAsync();
+            return Ok($"Successfully registered user:\n{userAuth}");
+        }
+
+        // Method to login a user.
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserAuth userAuth)
+        {
+            var user = await _context.UserAuths.FirstOrDefaultAsync(u => u.Username == userAuth.Username);
+            // TODO: Implement JWT
+            if (user != null && user.HashedPassword == userAuth.HashedPassword) return Ok($"Successfully logged in user:\n{userAuth}");
+            return NotFound($"There is no such user:\n{userAuth}.");
+        }
+
+        // Method to delete an account.
+
+    }
+}
---
+++ SecureNotesWebApi/Models/UserAuth.cs
@@ -0,0 +1,11 @@
+﻿namespace SecureNotesWebApi.Models
+{
+    public class UserAuth
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string HashedPassword { get; set; }
+        
+        //public string? PublicKey { get; set; }
+    }
+}
